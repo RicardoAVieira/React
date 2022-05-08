7c5b63cf9cfa8f73006333830892a9cf3bfd19d0
Ricardo Antunes Vieira <ricardo_av18@hotmail.com>
2022-05-08 19:44:45 -0300

    Aula 1 hooks

--- hooks/src/views/examples/UseEffect.jsx
+++ hooks/src/views/examples/UseEffect.jsx
@@ -1,63 +1,64 @@
-import React, { useEffect, useState } from 'react'
-import PageTitle from '../../components/layout/PageTitle'
-import SectionTitle from '../../components/layout/SectionTitle'
+import React, { useEffect, useState } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
-function calcFatorial(num) {
-    const n = parseInt(num)
-    if(n < 0) return -1
-    if(n === 0) return 1
-    return calcFatorial(n - 1) * n
+function calcularFatorial(num) {
+	const n = parseInt(num);
+	if (n < 0) return -1;
+	if (n === 0) return 1;
+	return calcularFatorial(n - 1) * n;
 }
 
 const UseEffect = (props) => {
+	const [number, setNumber] = useState(1);
+	const [fatorial, setFatorial] = useState(1);
 
-    // Ex #01
-    const [number, setNumber] = useState(1)
-    const [fatorial, setFatorial] = useState(1)
+	/* UseEffect funciona como se fosse um gatilho, sempre que uma determinada ação ocorrer ele chama a função descrita */
 
-    useEffect(function() {
-        setFatorial(calcFatorial(number))
-    }, [number])
+	useEffect(
+		function () {
+			setFatorial(calcularFatorial(number));
+		},
+		[number]
+	);
 
-    useEffect(function() {
-        if(fatorial > 1000000) {
-            document.title = "Eita!!!"
-        }
-    }, [fatorial])
+	useEffect(
+		function () {
+			if (fatorial > 1000000) {
+				document.title = "Eitaaaa !";
+			}
+		},
+		[fatorial]
+	);
 
-    // Ex #02
-    const [status, setStatus] = useState("Ímpar")
+	return (
+		<div className="UseEffect">
+			<PageTitle
+				title="Hook UseEffect"
+				subtitle="Permite executar efeitos colaterais em componentes funcionais!"
+			/>
+			<SectionTitle title="Exercício #01" />
+			<div className="center">
+				<div>
+					<span className="text">Fatorial:</span>
+					<span className="text red">
+						{fatorial === -1 ? "Não Existe" : fatorial}
+					</span>
+				</div>
+				<input
+					type="number"
+					className="input"
+					value={number}
+					onChange={(e) => setNumber(e.target.value)}
+				/>
+			</div>
 
-    useEffect(function() {
-        setStatus(number % 2 === 0 ? "Par" : "Ímpar")
-    }, [number])
-
-    return (
-        <div className="UseEffect">
-            <PageTitle
-                title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!" />
-
-            <SectionTitle title="Exercício #01"/>
+			<SectionTitle title="Exercício #02" />
             <div className="center">
-                <div>
-                    <span className="text">Fatorial: </span>
-                    <span className="text red">{fatorial === -1 ? 'Não existe' :  fatorial}</span>
-                </div>
-                <input type="number" className="input"
-                    value={number}
-                    onChange={e => setNumber(e.target.value)} />
+                
             </div>
+		</div>
+	);
+};
 
-            <SectionTitle title="Exercício #02"/>
-            <div className="center">
-                <div>
-                    <span className="text">Status: </span>
-                    <span className="text red">{status}</span>
-                </div>
-            </div>
-        </div>
-    )
-}
-
-export default UseEffect
+export default UseEffect;
