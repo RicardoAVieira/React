b72114c3cb98b754e713a28b707b86c3eb91e078
Ricardo Antunes Vieira <ricardo_av18@hotmail.com>
2022-05-10 14:47:31 -0300

    Aula Context 1.

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -9,8 +9,7 @@ import DataContext, { data } from "./data/DataContext";
 ReactDOM.render(
 	/* Esse Componente emvolve todos os outros componentes presentes em app e fornece a todos
     o componente data, este componente contem um texto e um numero */
-	<DataContext.Provider value={data}>
-		<App />
-	</DataContext.Provider>,
+
+	<App />,
 	document.getElementById("root")
 );
--- hooks/src/views/examples/UseContext.jsx
+++ hooks/src/views/examples/UseContext.jsx
@@ -6,10 +6,19 @@ import DataContext from "../../data/DataContext";
 import { AppContext } from "../../data/Store";
 
 const UseContext = (props) => {
-	const context =
+	const { state, setState } =
 		useContext(
 			DataContext
 		); /* Criação de um contexto a partir da importação do DataContext */
+
+	function addNumber(delta) {
+		setState({
+			/* Desta forma, carregamos o objeto com o Spread, e então substitui apenas o number,
+			se este passo não for implementado, ao alterar o number, na verdade ele sobreescreve todo o resto. */
+			...state,
+			number: state.number + delta,
+		});
+	}
 
 	return (
 		<div className="UseContext">
@@ -17,11 +26,19 @@ const UseContext = (props) => {
 				title="Hook UseContext"
 				subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
 			/>
-            <div className="center">
-                <span>Teste</span>
-                <span className="text">{context.text}</span>
-                <span className="text">{context.number}</span>
-            </div>
+			<SectionTitle title="Exercício #01" />
+			<div className="center">
+				<span className="text">{state.text}</span>
+				<span className="text">{state.number}</span>
+				<div>
+					<button className="btn" onClick={() => addNumber(-1)}>
+						-1
+					</button>
+					<button className="btn" onClick={() => addNumber(+1)}>
+						+1
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 };
